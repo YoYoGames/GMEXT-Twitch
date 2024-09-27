5d5cb8626af6f7d9782becf53ba9312e5207a09b
YYBartT <bteunis@opera.com>
2024-09-27 15:38:35 +0200

    docs(general): more docs fixes, added sidebar

--- docs/Docs.js
+++ docs/Docs.js
@@ -545,7 +545,7 @@ function twitch_bits_get_bits_leaderboard(optionals, callback_success, callback_
  * 
  * [[Note: This requires an [app access token](https://dev.twitch.tv/docs/authentication#app-access-tokens) or [user access token](https://dev.twitch.tv/docs/authentication#user-access-tokens).]]
  * 
- * @param {string} broadcaster_id` ${type.string} The ID of the broadcaster whose custom Cheermotes you want to get. Specify the broadcaster's ID if you want to include the broadcaster's Cheermotes in the response (not all broadcasters upload Cheermotes). If not specified, the response contains only global Cheermotes.
+ * @param {string} broadcaster_id The ID of the broadcaster whose custom Cheermotes you want to get. Specify the broadcaster's ID if you want to include the broadcaster's Cheermotes in the response (not all broadcasters upload Cheermotes). If not specified, the response contains only global Cheermotes.
  * 
  * If the broadcaster uploaded Cheermotes, the `type` field in the response is set to `"channel_custom"`.
  * 
@@ -1588,8 +1588,38 @@ function twitch_chat_get_user_emotes(user_id, optionals, callback_success, callb
  * 
  * [[Note: Requires a [user access token](https://dev.twitch.tv/docs/authentication#user-access-tokens) that includes the ${constant.TWITCH_SCOPE}: `TWITCH_SCOPE_MODERATOR_MANAGE_CHAT_SETTINGS`.]]
  * 
+ * To set the `slow_mode_wait_time` or `follower_mode_duration` field to its default value, set the corresponding `slow_mode` or `follower_mode` field to `true` (and don't include the `slow_mode_wait_time` or `follower_mode_duration` field).
+ * 
+ * To set the `slow_mode_wait_time`, `follower_mode_duration`, or `non_moderator_chat_delay_duration` field's value, you must set the corresponding `slow_mode`, `follower_mode`, or `non_moderator_chat_delay` field to `true`.
+ * 
+ * To remove the `slow_mode_wait_time`, `follower_mode_duration`, or `non_moderator_chat_delay_duration` field's value, set the corresponding `slow_mode`, `follower_mode`, or `non_moderator_chat_delay` field to `false` (and don't include the `slow_mode_wait_time`, `follower_mode_duration`, or `non_moderator_chat_delay_duration` field).
+ * 
  * @param {string} broadcaster_id The ID of the broadcaster whose chat settings you want to update. 
  * @param {string} moderator_id The ID of a user that has permission to moderate the broadcaster's chat room, or the broadcaster's ID if they're making the update. This ID must match the user ID in the user access token. 
+ * @param {struct} optionals The optional parameters to be passed into the function:
+ * 
+ * - `emote_mode` : ${type.bool} : A Boolean value that determines whether chat messages must contain only emotes.
+ *   Set to `true` if only emotes are allowed; otherwise, `false`. The default is `false`.
+ * - `follower_mode` : ${type.bool} : A Boolean value that determines whether the broadcaster restricts the chat room to followers only.
+ *   Set to `true` if the broadcaster restricts the chat room to followers only; otherwise, `false`. The default is `true`.
+ *   To specify how long users must follow the broadcaster before being able to participate in the chat room, see the `follower_mode_duration` field.
+ * - `follower_mode_duration` : ${type.bool} : The length of time, in minutes, that users must follow the broadcaster before being able to participate in the chat room. Set only if `follower_mode` is `true`. Possible values are: 0 (no restriction) through 129600 (3 months). The default is 0.
+ * - `non_moderator_chat_delay` : ${type.bool} : A Boolean value that determines whether the broadcaster adds a short delay before chat messages appear in the chat room. This gives chat moderators and bots a chance to remove them before viewers can see the message.
+ *   Set to `true` if the broadcaster applies a delay; otherwise, `false`. The default is `false`.
+ *   To specify the length of the delay, see the `non_moderator_chat_delay_duration` field.
+ * - `non_moderator_chat_delay_duration` : ${type.real} : The amount of time, in seconds, that messages are delayed before appearing in chat. Set only if `non_moderator_chat_delay` is `true`. Possible values are:
+ *   * 2 - 2 second delay (recommended)
+ *   * 4 - 4 second delay
+ *   * 6 - 6 second delay
+ * - `slow_mode` : ${type.bool} : A Boolean value that determines whether the broadcaster limits how often users in the chat room are allowed to send messages. Set to `true` if the broadcaster applies a wait period between messages; otherwise, `false`. The default is `false`.
+ *   To specify the delay, see the `slow_mode_wait_time` field.
+ * - `slow_mode_wait_time` : ${type.real} : The amount of time, in seconds, that users must wait between sending messages. Set only if `slow_mode` is `true`.
+ *   Possible values are: 3 (3 second delay) through 120 (2 minute delay). The default is 30 seconds.
+ * - `subscriber_mode` : ${type.bool} : A Boolean value that determines whether only users that subscribe to the broadcaster’s channel may talk in the chat room.
+ *   Set to `true` if the broadcaster restricts the chat room to subscribers only; otherwise, `false`. The default is `false`.
+ * - `unique_chat_mode` : ${type.bool} : A Boolean value that determines whether the broadcaster requires users to post only unique messages in the chat room.
+ *   Set to `true` if the broadcaster allows only unique messages; otherwise, `false`. The default is `false`.
+ * 
  * @param {function} callback_success Triggered if the request succeeded
  * @param {function} callback_failed Triggered if the request failed
  * @event callback_success
@@ -2860,6 +2890,8 @@ function twitch_eventsub_get_eventsub_subscriptions(optionals, callback_success,
  * [[Note: Ping-pong messages are handled automatically by GameMaker.]]
  * 
  * @param {function} callback The callback function that's called when data is received on the network socket
+ * 
+ * @returns {real}
  * 
  * @func_end
  */
